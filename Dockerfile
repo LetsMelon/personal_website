1c1d1daf2bb5b9781ffc6ecc560ab0d108490e3a
LetsMelon <domi.m@outlook.com>
2023-09-03 22:27:30 +0200

    use docker buildkit to cache rust compiling

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,20 @@
+# syntax=docker/dockerfile:1.2
 FROM alpine:3.17 as BLOG_BUILDER
 
 RUN apk add rust cargo minify
-
-RUN cargo new test_project && cd test_project && cargo add anyhow && cargo b && cd .. && rm -r /test_project
 
 WORKDIR /html-site-generator
 
 COPY ["Cargo.toml", "Cargo.lock", "./"]
 COPY ./src ./src
 
-RUN cargo b
-RUN mkdir ./dst && ./target/debug/melcher_io
+RUN --mount=type=cache,target=/root/.cargo/git \
+    --mount=type=cache,target=/root/.cargo/registry \
+    --mount=type=cache,sharing=private,target=/html-site-generator/target \
+    cargo build --release && \
+    cp target/release/melcher_io /bin/melcher_io
+
+RUN mkdir ./dst && /bin/melcher_io
 
 FROM nginx:1.25-alpine
 
---
+++ src/widget/ferris.rs
@@ -0,0 +1,59 @@
+use std::sync::atomic::{AtomicBool, Ordering};
+
+use html_site_generator::attributes::SetHtmlAttributes;
+use html_site_generator::html::image::{Image, ImageBuilder};
+use html_site_generator::html::IntoHtmlNode;
+
+#[derive(Debug)]
+pub struct Ferris {
+    inner: Image,
+}
+
+impl Ferris {
+    pub fn new() -> Self {
+        let mut i = ImageBuilder::default()
+            .src("/assets/ferris.svg")
+            .alt("Rust logo")
+            .build()
+            .unwrap();
+        i.set_id("rust-lang-icon");
+
+        Ferris { inner: i }
+    }
+}
+
+impl IntoHtmlNode for Ferris {
+    fn transform_into_raw_html(&self, buffer: &mut dyn std::io::Write) -> anyhow::Result<()> {
+        self.inner.transform_into_raw_html(buffer)?;
+
+        Ok(())
+    }
+
+    fn transform_into_raw_css(&self, buffer: &mut dyn std::io::Write) -> anyhow::Result<()> {
+        self.inner.transform_into_raw_css(buffer)?;
+
+        // only write the css once, this can also be created with an unique link
+        static WROTE_CSS: AtomicBool = AtomicBool::new(false);
+        if !WROTE_CSS.load(Ordering::Relaxed) {
+            writeln!(
+                buffer,
+                "#rust-lang-icon {{
+    height: 1em;
+    position: relative;
+    top: 0.125em;
+    left: 0.125em;
+}}"
+            )?;
+
+            WROTE_CSS.store(true, Ordering::Relaxed);
+        }
+
+        Ok(())
+    }
+
+    fn transform_into_raw_js(&self, buffer: &mut dyn std::io::Write) -> anyhow::Result<()> {
+        self.inner.transform_into_raw_js(buffer)?;
+
+        Ok(())
+    }
+}
