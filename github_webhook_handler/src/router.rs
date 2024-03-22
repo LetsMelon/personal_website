68f506ef09d7fe9c9a7322407e502696610aef56
LetsMelon <domi.m@outlook.com>
2024-03-22 18:47:59 +0100

    add middleware for trace, cors and compression in webhook handler

--- github_webhook_handler/src/router.rs
+++ github_webhook_handler/src/router.rs
@@ -1,8 +1,13 @@
 use axum::body::Bytes;
-use axum::http::{HeaderMap, StatusCode};
+use axum::http::{HeaderMap, Method, StatusCode};
 use axum::routing::{get, post};
-use axum::Router;
+use axum::{Json, Router};
+use serde_json::json;
 use tokio::sync::mpsc::Sender;
+use tower::ServiceBuilder;
+use tower_http::compression::CompressionLayer;
+use tower_http::cors::CorsLayer;
+use tower_http::trace::TraceLayer;
 
 use crate::github::GitHubWebhook;
 
@@ -50,5 +55,25 @@ pub fn router(secret_token: String, payload_sender: Sender<GitHubWebhook>) -> Ro
                 tracing::info!("Return status code");
                 (StatusCode::ACCEPTED, "Accepted")
             }),
+        )
+        .fallback(|| async {
+            let status_code = StatusCode::NOT_FOUND;
+
+            (
+                status_code,
+                Json(json!({
+                    "status": status_code.as_u16(), "message": status_code.canonical_reason()
+                })),
+            )
+        })
+        .layer(
+            ServiceBuilder::new()
+                .layer(TraceLayer::new_for_http())
+                .layer(
+                    CorsLayer::new()
+                        .allow_methods([Method::GET, Method::POST])
+                        .allow_origin(tower_http::cors::Any),
+                )
+                .layer(CompressionLayer::new()),
         )
 }
