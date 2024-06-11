87b2b9f1284d3f18213810d2343eda1628a4c6c6
LetsMelon <domi.m@outlook.com>
2024-06-11 16:06:58 +0200

    add footer

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ComponentsModule } from './components/components.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, ComponentsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
