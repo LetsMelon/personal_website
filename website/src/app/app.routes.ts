d2f0ba758f13133e8db22eddbf76516ca05177a7
LetsMelon <domi.m@outlook.com>
2024-06-11 23:56:17 +0200

    implement blogs

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { BlogsComponent } from './pages/blogs/blogs.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'blogs',
+    component: BlogsComponent,
   },
 ];
