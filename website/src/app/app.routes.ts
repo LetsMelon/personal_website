ef83c087ac9425640dcd5c8544851756b7a08270
LetsMelon <domi.m@outlook.com>
2024-06-19 23:13:01 +0200

    generate component for projects

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { BlogsComponent } from './pages/blogs/blogs.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'blogs',
     component: BlogsComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
   },
 ];
