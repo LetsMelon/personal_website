1823bad2f0d579533d60e933b2c661f1d994c13e
LetsMelon <domi.m@outlook.com>
2024-07-23 20:23:03 +0200

    remove angular website

--- website/src/app/components/separator/separator.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-separator',
-  templateUrl: './separator.component.html',
-  styleUrl: './separator.component.scss',
-})
-export class SeparatorComponent {}
