191269fe18f6e1771335ee9a3e4fabfd248caee8
LetsMelon <domi.m@outlook.com>
2024-07-23 20:23:03 +0200

    remove angular website

--- website/src/app/components/ferris/ferris.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-ferris',
-  templateUrl: './ferris.component.html',
-  styleUrl: './ferris.component.scss',
-})
-export class FerrisComponent {}
