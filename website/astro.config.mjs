00418e6698299a88d9280157dbfa2e2bfd251c30
LetsMelon <domi.m@outlook.com>
2024-07-27 15:58:27 +0200

    add @astrojs/sitemap to project

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://melcher.io",
-
-  integrations: [
-    robotsTxt({
-      sitemap: false,
-    }),
-  ],
+  integrations: [sitemap(), robotsTxt()],
 });
